81b32418e9c7fd56b0653ab234d26f325ae105f5
JosephIgbaji <igbajijoseph.k@gmail.com>
2025-02-11 13:49:16 +0100

    Login and register route updated 100% completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,10 @@ import User from "../models/User.js";
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(401).json({ error: "Email Already Exist" });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
@@ -15,16 +19,21 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  console.log(req.body);
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: "user does not exist" });
+    }
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.json({ token });
+    user.token = token;
+    res.status(200).json({ data: { ...user._doc }, token });
   } catch (error) {
     res.status(500).json({ error: "Error logging in" });
   }
